627476218a4bd99b858f8ed4a88060ac1fcbf36e
lavender agwena <abigaillavender238@gmail.com>
2024-05-11 03:53:27 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var typed=new Typed(".typing",{
+    strings:["  Front end Web Designer"],
+    typedSpeed:100,
+    Backspeed:-10,
+    loop:true,
+})
+const navToggler=document.querySelector(".nav-toggler");
+navToggler.forEach((toggle)=>navToggler.addEventListener("click"),()=>{
+    ul.classList.remove("show")
+})
